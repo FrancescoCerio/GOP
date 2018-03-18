e5106cf04daf6a2d348aceed0894da23b3699dc7
francesco_cerio <francescocerio98@gmail.com>
2018-03-18 19:38:36 +0100

    Create dado.hpp

---
+++ src/dado/dado.hpp
@@ -0,0 +1,31 @@
+//
+//  Dado.hpp
+//  GOP
+//
+//  Created by Francesco Cerio on 18/03/18.
+//  Copyright © 2018 Francesco Cerio. All rights reserved.
+//
+
+#ifndef Dado_hpp
+#define Dado_hpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+class Dado{
+public:
+    
+    int d;
+
+    Dado();
+    
+    void lanciaDado();
+
+};
+
+
+#endif /* Dado_hpp */
